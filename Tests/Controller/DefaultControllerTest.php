74d3ef959efdec626a828e5e0b97325444d9f53b
Alex <xedinaska@gmail.com>
2014-04-07 20:57:37 +0300

    Added tests for controller and backup service

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -4,14 +4,29 @@ namespace Xedinaska\BackupsManagerBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class DefaultControllerTest
+ * Check test controller created for bundle
+ *
+ * @package Xedinaska\BackupsManagerBundle\Tests\Controller
+ */
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * Check that page available
+     *
+     * @access public
+     * @return void
+     */
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $client->request('GET', '/backups');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $responseCode = $client->getResponse()
+            ->getStatusCode();
+
+        $this->assertTrue($responseCode == 200);
     }
 }
